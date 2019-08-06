108068563f0e2fb1fb4164512326b9457f704c86
aehtiopicus <javierv@eventbrite.com>
2019-08-06 14:09:42 -0300

    changes

--- RangeDatepicker/index.js
+++ RangeDatepicker/index.js
@@ -314,23 +314,4 @@ const styles = StyleSheet.create({
 	},
 });
 
-const styles = StyleSheet.create({
-	dayHeader: {
-		flexDirection: 'row',
-		borderBottomWidth: 1,
-		paddingBottom: 10,
-		paddingTop: 10,
-	},
-	buttonWrapper: {
-		paddingVertical: 10,
-		paddingHorizontal: 15,
-		backgroundColor: 'white',
-		borderTopWidth: 1,
-		borderColor: '#ccc',
-
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-	},
-});
-
 /* eslint-enable  */
